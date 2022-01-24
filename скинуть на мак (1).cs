6d2a3caf106a901124bc0906843315fddcb73c3c
maksimmer348 <maksimmer348@gmail.com>
2022-01-24 18:03:38 +0300

    add декоратор, фасад

--- скинуть на мак (1).cs
+++ скинуть на мак (1).cs
@@ -13059,6 +13059,334 @@ class Client
 
 
 
+using System.Reflection.Metadata.Ecma335;
+
+public abstract class Person//определяет базовую функциональность
+{
+    public string Name { get; private set; }
+    protected Person(string name)
+    {
+        Name = name;
+    }
+    public abstract string Dress();
+    
+}
+
+// конкретная реализация компонента Person, в которую с помощью декоратора добавляется
+// новая функциональность
+public class PersonOne : Person
+{
+    public PersonOne(string name) : base(name)
+    {
+    }
+
+    public override string Dress()
+    {
+        return $"{Name} одел штаны";
+    }
+}
+
+// конкретная реализация компонента Person, в которую с помощью декоратора добавляется
+// новая функциональность
+public class PersonTwo : Person
+{
+    public PersonTwo(string name) : base(name)
+    {
+    }
+    public override string Dress()
+    {
+        return $"{Name} одел трусы";
+    }
+}
+
+//собственно декоратор, реализуется в виде абстрактного класса и имеет тот же базовый класс,
+//что и декорируемые объекты. Поэтому базовый класс Person должен быть по возможности
+//легким и определять только базовый интерфейс.
+public abstract class PersonDecorator : Person
+{
+    //Класс декоратора также хранит ссылку на декорируемый объект в виде объекта базового
+    //класса Component и реализует связь с базовым классом как через наследование,
+    //так и через отношение агрегации.
+    public Person MyPerson;
+    protected PersonDecorator(string name, Person person) : base(name)
+    {
+        MyPerson = person;
+    }
+}
+
+//Класс PersonPullover представляет дополнительную функциональность,
+//которой должен быть расширен объект PersoneOe или PersoneTwo
+public class PersonPullover : PersonDecorator
+{
+    public PersonPullover(Person person) : base(person.Name , person)
+    {
+    }
+
+    public override string Dress()
+    {
+        return MyPerson.Dress() + ", одел свитер";
+    }
+}
+
+//Класс PersonPullover представляет дополнительную функциональность,
+//которой должен быть расширен объект PersoneOe или PersoneTwo
+public class PersonShurt : PersonDecorator
+{
+    public PersonShurt(Person person) :  base(person.Name , person)
+    {
+    }
+
+    public override string Dress()
+    {
+        return MyPerson.Dress() + ", одел рубашку";
+    }
+}
+
+
+
+Person person2 = new PersonTwo("Lex");
+
+Console.WriteLine(person2.Dress());//базовая функциональность (Lex одел трусы)
+
+
+//добавляем новые обертки для класса, достаточно унаследовать от декоратора класс,
+//который будет определять дополнительный функционал.
+person2 = new PersonPullover(person2);
+person2 = new PersonShurt(person2);
+
+
+Console.WriteLine(person2.Dress());//Lex одел трусы, одел свитер, одел рубашку
+
+---------------
+
+namespace RefactoringGuru.DesignPatterns.Composite.Conceptual
+{
+    // Базовый интерфейс Компонента определяет поведение, которое изменяется
+    // декораторами.
+    public abstract class Component
+    {
+        public abstract string Operation();
+    }
+
+    // Конкретные Компоненты предоставляют реализации поведения по умолчанию.
+    // Может быть несколько вариаций этих классов.
+    class ConcreteComponent : Component
+    {
+        public override string Operation()
+        {
+            return "ConcreteComponent";
+        }
+    }
+
+    // Базовый класс Декоратора следует тому же интерфейсу, что и другие
+    // компоненты. Основная цель этого класса - определить интерфейс обёртки для
+    // всех конкретных декораторов. Реализация кода обёртки по умолчанию может
+    // включать в себя  поле для хранения завёрнутого компонента и средства его
+    // инициализации.
+    abstract class Decorator : Component
+    {
+        protected Component _component;
+
+        public Decorator(Component component)
+        {
+            this._component = component;
+        }
+
+        public void SetComponent(Component component)
+        {
+            this._component = component;
+        }
+
+        // Декоратор делегирует всю работу обёрнутому компоненту.
+        public override string Operation()
+        {
+            if (this._component != null)
+            {
+                return this._component.Operation();
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+    }
+
+    // Конкретные Декораторы вызывают обёрнутый объект и изменяют его результат
+    // некоторым образом.
+    class ConcreteDecoratorA : Decorator
+    {
+        public ConcreteDecoratorA(Component comp) : base(comp)
+        {
+        }
+
+        // Декораторы могут вызывать родительскую реализацию операции, вместо
+        // того, чтобы вызвать обёрнутый объект напрямую. Такой подход упрощает
+        // расширение классов декораторов.
+        public override string Operation()
+        {
+            return $"ConcreteDecoratorA({base.Operation()})";
+        }
+    }
+
+    // Декораторы могут выполнять своё поведение до или после вызова обёрнутого
+    // объекта.
+    class ConcreteDecoratorB : Decorator
+    {
+        public ConcreteDecoratorB(Component comp) : base(comp)
+        {
+        }
+
+        public override string Operation()
+        {
+            return $"ConcreteDecoratorB({base.Operation()})";
+        }
+    }
+    
+    public class Client
+    {
+        // Клиентский код работает со всеми объектами, используя интерфейс
+        // Компонента. Таким образом, он остаётся независимым от конкретных
+        // классов компонентов, с которыми работает.
+        public void ClientCode(Component component)
+        {
+            Console.WriteLine("RESULT: " + component.Operation());
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+
+            var simple = new ConcreteComponent();
+            Console.WriteLine("Client: I get a simple component:");
+            client.ClientCode(simple);
+            Console.WriteLine();
+
+            // ...так и декорированные.
+            //
+            // Обратите внимание, что декораторы могут обёртывать не только
+            // простые компоненты, но и другие декораторы.
+            ConcreteDecoratorA decorator1 = new ConcreteDecoratorA(simple);
+            ConcreteDecoratorB decorator2 = new ConcreteDecoratorB(decorator1);
+            Console.WriteLine("Client: Now I've got a decorated component:");
+            client.ClientCode(decorator2);
+        }
+    }
+}
+
+
+--Фасад--Facade
+------------------------------------------
+если коду приходится работать с большим колвом обьектов некой сложной билиотеки или фарймворка 
+нужно самостоятльно инициализировать эти оьекты следаить за правильным порядком зависимостей и тд
+
+когда исползовать -когда имеется сложная сисетма, и необходимо упростиь с ней работу. фасад позволит определеить
+одну точку взаимодействия между клиентом и системой. - кода нужно уменшить колво зависимостей между клиентом и 
+сложной ситемой, фасадные обьекты позволяют отоделить, изолировать копмпоненты системы от клиента и разививать и 
+рабоатьа с ними независимо. -когда нужно определеить подсистемы компонентов с сложной системе. создание фасадов
+каждой подсистеы что позволит упростить их взаимодействие и повысытиь независимость друг от друга.
+
+namespace ConsoleApp1;
+
+#region компоненты сложной системы с которой должен взаимодействоваьт клиен
+
+public class TextEditor
+{
+    public void CreateCode()
+    {
+        Console.WriteLine("Создание кода");   
+    }
+
+    public void Save()
+    {
+        Console.WriteLine("Сохранение кода");   
+    }
+}
+
+public class  Compiler
+{
+    public void Compile()
+    {
+        Console.WriteLine("Компиляция приложеиня");
+    }
+}
+
+public class Clr
+{
+    public void Execute()
+    {
+        Console.WriteLine("Выполнение приложения");
+    }
+
+    public void Finish()
+    {
+        Console.WriteLine("Завершение работы приложения");
+    }
+}
+
+#endregion
+
+//фасад непосродсвенно ктороый представолят интерфей клинету для работы с компонентами
+public class VisualStudioFacade
+{
+    private TextEditor textEditor;
+    private Compiler compiler;
+    private Clr clr;
+
+    public VisualStudioFacade(TextEditor textEditor, Compiler compiler, Clr clr)
+    {
+        this.textEditor = textEditor;
+        this.compiler = compiler;
+        this.clr = clr;
+    }
+
+    public void Start()
+    {
+        textEditor.CreateCode();
+        textEditor.Save();
+        compiler.Compile();
+        clr.Execute();
+    }
+
+    public void Stop()
+    {
+        clr.Finish();
+    }
+}
+
+//взаимодействует со сложными компнентами подсистемы 
+public class ClientProgrammer
+{
+    //взаимодействие через класс фасад который скрывает сложности реализации от
+    //пользователя
+    public void WorkInVisualStudio(VisualStudioFacade facade)
+    {
+        facade.Start();
+        facade.Stop();
+    }
+}
+
+
+TextEditor textEditor = new TextEditor();
+Compiler compiler = new Compiler();
+Clr clr = new Clr();
+
+VisualStudioFacade ide = new VisualStudioFacade(textEditor, compiler, clr);
+ClientProgrammer clientProgrammer = new ClientProgrammer();
+clientProgrammer.WorkInVisualStudio(ide);
+
+
+клиент может при необходимости обращаться напрямую к компонентам, например, отдельно от других компонентов 
+использовать текстовый редактор. Но в виду сложности процесса создания приложения лучше использовать фасад. 
+Также это не единственный возможный фасад для работы с данными компонентами. При необходимости можно создавать
+альтернативные фасады также, как в реальной жизни мы можем использовать альтернативные среды разработки.
+
+
+
+
+
 
 --mvc--
 ------------------------------------------
@@ -13070,6 +13398,8 @@ class Client
 которое предоставляет данные полльзователю, не доллжны знать друг у друге или взаимодействовоат друг с другом напрямую. 
 контроллер как бы посредник, напрмиер контроллер принимает ввод от пользователя такой как нажатие кнопки, и информирует модель о 
 необходимости предпринять соотвествующие деействия кароче контроллер берет нажаите кнопки и вызыввает приязаный к ней(нажатие кнопки) метод.
+
+
 
 
 
